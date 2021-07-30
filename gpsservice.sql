ef9bf6a4c0f6f4e85eca31260df55239912509ca
sjml2002 <sjml2002@naver.com>
2021-07-30 14:33:14 +0900

    [2021/07/30] DB변경

--- gpsservice.sql
+++ gpsservice.sql
@@ -1,78 +1,83 @@
--- MariaDB dump 10.19  Distrib 10.4.19-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: gpsservice
--- ------------------------------------------------------
--- Server version	10.4.19-MariaDB
+-- Host: 127.0.0.1
+-- 생성 시간: 21-07-30 07:32
+-- 서버 버전: 10.4.20-MariaDB
+-- PHP 버전: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `location`
+-- 데이터베이스: `gpsservice`
 --
 
-DROP TABLE IF EXISTS `location`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `location`
+--
+
 CREATE TABLE `location` (
   `latitude` float NOT NULL,
-  `longitude` float NOT NULL
+  `longitude` float NOT NULL,
+  `age` int(2) NOT NULL,
+  `gender` int(1) NOT NULL,
+  `job` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `location`
+-- 테이블의 덤프 데이터 `location`
 --
 
-LOCK TABLES `location` WRITE;
-/*!40000 ALTER TABLE `location` DISABLE KEYS */;
-INSERT INTO `location` VALUES (37.422,-122.084);
-/*!40000 ALTER TABLE `location` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `location` (`latitude`, `longitude`, `age`, `gender`, `job`) VALUES
+(37.4219, -122.084, 20, 1, 'student'),
+(37.4219, -122.084, 20, 1, 'student'),
+(37.422, -122.084, 20, 1, 'student'),
+(37.422, -122.084, 20, 1, 'student');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `userinfo`
+-- 테이블 구조 `userinfo`
 --
 
-DROP TABLE IF EXISTS `userinfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userinfo` (
   `id` varchar(21) NOT NULL,
   `pw` varchar(255) NOT NULL,
   `age` int(2) NOT NULL,
   `gender` int(1) NOT NULL,
   `job` varchar(10) NOT NULL,
-  `salt` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `salt` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `userinfo`
+-- 테이블의 덤프 데이터 `userinfo`
 --
 
-LOCK TABLES `userinfo` WRITE;
-/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
-INSERT INTO `userinfo` VALUES ('js2002','$2y$10$8lEA/pFIgf4w5YDFiRWfueLAkeyv62UBKFLxJdoJPUuflG9ZJMP0S',20,1,'student','89414f9ef28fc378803c580bc7385a66cd02b0a7ff4890a448f3933e4cafa745');
-/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `userinfo` (`id`, `pw`, `age`, `gender`, `job`, `salt`) VALUES
+('js2002', '$2y$10$8lEA/pFIgf4w5YDFiRWfueLAkeyv62UBKFLxJdoJPUuflG9ZJMP0S', 20, 1, 'student', '89414f9ef28fc378803c580bc7385a66cd02b0a7ff4890a448f3933e4cafa745');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `userinfo`
+--
+ALTER TABLE `userinfo`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-06-26 13:52:40
