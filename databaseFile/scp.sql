d578d84919d35f3c61e2cdc0c93163d3a3dd60a0
김준상 <sjml2002@naver.com>
2023-01-08 01:50:26 +0000

    [2023/01/08] start!

--- databaseFile/scp.sql
+++ databaseFile/scp.sql
@@ -1,100 +1,85 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.40, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- 생성 시간: 21-06-18 22:35
--- 서버 버전: 10.4.17-MariaDB
--- PHP 버전: 8.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: scp
+-- ------------------------------------------------------
+-- Server version	5.7.40-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- 데이터베이스: `scp`
+-- Table structure for table `projecttable`
 --
 
--- --------------------------------------------------------
-
---
--- 테이블 구조 `projecttable`
---
-
+DROP TABLE IF EXISTS `projecttable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `projecttable` (
-  `no` int(11) UNSIGNED NOT NULL,
+  `no` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `writer` varchar(21) CHARACTER SET utf8mb4 NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `hit` int(10) NOT NULL,
   `content` text CHARACTER SET utf8mb4 NOT NULL,
   `img_name` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
   `category` int(2) NOT NULL,
   `one_or_company` int(2) NOT NULL,
   `age` int(4) NOT NULL,
-  `region` varchar(18) CHARACTER SET utf8mb4 DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `region` varchar(18) CHARACTER SET utf8mb4 DEFAULT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블의 덤프 데이터 `projecttable`
+-- Dumping data for table `projecttable`
 --
 
-INSERT INTO `projecttable` (`no`, `writer`, `date`, `hit`, `content`, `img_name`, `category`, `one_or_company`, `age`, `region`) VALUES
-(9, 'js2002', '2021-06-18 06:34:58', 0, '아니 이게 그니까 왜 안돼냐고 사발', 'C:\\fakepath\\민성_백준프로필.jpg', 1, 1, 1, 'seoul'),
-(10, 'js2002', '2021-06-18 06:38:55', 0, '이거 안되면 진짜 시발', 'C:\\fakepath\\준상_백준프로필.png', 1, 1, 1, 'seoul');
-
--- --------------------------------------------------------
+LOCK TABLES `projecttable` WRITE;
+/*!40000 ALTER TABLE `projecttable` DISABLE KEYS */;
+INSERT INTO `projecttable` VALUES (9,'js2002','2021-06-18 06:34:58',0,'아니 이게 그니까 왜 안돼냐고 사발','C:\\fakepath\\민성_백준프로필.jpg',1,1,1,'seoul'),(10,'js2002','2021-06-18 06:38:55',0,'이거 안되면 진짜 시발','C:\\fakepath\\준상_백준프로필.png',1,1,1,'seoul');
+/*!40000 ALTER TABLE `projecttable` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 테이블 구조 `user_info`
+-- Table structure for table `user_info`
 --
 
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_info` (
   `id` varchar(100) NOT NULL,
   `pw` varchar(100) NOT NULL,
   `salt` varchar(100) NOT NULL,
-  `sessionToken` varchar(100) DEFAULT NULL
+  `sessionToken` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 테이블의 덤프 데이터 `user_info`
+-- Dumping data for table `user_info`
 --
 
-INSERT INTO `user_info` (`id`, `pw`, `salt`, `sessionToken`) VALUES
-('js2002', '$2y$10$c5XwSZ3vviI4LyQqkAUfmuV8LfqL5b10UVLUQaZaS8RHFL/E/bVcC', '63b5abb0540afc50487d641b60bcc06f7a63f4facb0546ce043649fe5fa39216', '108a61181bc82080267d8236f7339d87');
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES ('js2002','$2y$10$FQbSROAanK2ka04tzfaimuegCp.ZajdbIqxJn7ejQog/1dXpSmX4a','8fe34a61747bb20f106fd364502ff8de987b4b89d34550c30722c53f50ddf934','');
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- 덤프된 테이블의 인덱스
---
-
---
--- 테이블의 인덱스 `projecttable`
---
-ALTER TABLE `projecttable`
-  ADD PRIMARY KEY (`no`);
-
---
--- 테이블의 인덱스 `user_info`
---
-ALTER TABLE `user_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- 덤프된 테이블의 AUTO_INCREMENT
---
-
---
--- 테이블의 AUTO_INCREMENT `projecttable`
---
-ALTER TABLE `projecttable`
-  MODIFY `no` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-08  1:48:22
